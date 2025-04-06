17bce1f5d1340c1eb9a638e28a6af051067ee1c0
prajwal89 <22263436+prajwal89@users.noreply.github.com>
2025-04-06 19:26:18 +0530

    add NavigationGroup

--- src/AnalyticPlugin.php
+++ src/AnalyticPlugin.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Prajwal89\Analytics;
 
 use Filament\Contracts\Plugin;
+use Filament\Navigation\NavigationGroup;
 use Filament\Panel;
 use Prajwal89\Analytics\Filament\Resources\BiasResource;
 use Prajwal89\Analytics\Filament\Resources\PageViewResource;
@@ -22,6 +23,12 @@ class AnalyticPlugin implements Plugin
             ->resources([
                 PageViewResource::class,
                 BiasResource::class,
+            ])
+            ->navigationGroups([
+                NavigationGroup::make()
+                    ->label('Analytics')
+                    ->icon('heroicon-o-chart-bar')
+                    ->collapsed(),
             ])
             ->pages([
                 // Settings::class,
