ce542f75115d582168a031e072f9e44175a9bedb
prajwal89 <22263436+prajwal89@users.noreply.github.com>
2025-04-14 20:53:03 +0530

    attach version query

--- resources/views/track.blade.php
+++ resources/views/track.blade.php
@@ -1,2 +1,3 @@
 {{-- todo: use blade directive --}}
-<script src="{{ asset('/vendor/prajwal89/analytics/an.js') }}" defer></script>
+{{-- todo: attack version query string automatically --}}
+<script src="{{ asset('/vendor/prajwal89/analytics/an.js?v=0.1.1') }}" defer></script>
